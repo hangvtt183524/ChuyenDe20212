260e82043a4057ef88ca408b5bdd865db53007f4
bva <hochochoc999@gmail.com>
2022-05-30 01:58:03 +0700

    build Button component

---
+++ PetCare-Portal/src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+    <a href="href" class="button">
+    </a>
+</template>
+
+<script>
+
+export default {
+  props: {
+    href: {
+      type: String,
+      default: null,
+    },
+  },
+};
+
+</script>
