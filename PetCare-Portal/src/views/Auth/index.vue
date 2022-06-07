d24fa62a8b1a93755c1d320c01897e0b6a6eff5c
NinhNghia <nghianinhnb@gmail.com>
2022-06-07 19:43:19 +0700

    .

--- PetCare-Portal/src/views/Auth/index.vue
+++ PetCare-Portal/src/views/Auth/index.vue
@@ -13,10 +13,10 @@ export default {
 <template>
     <div class="container pb-5">
       <div class="row">
-        <div class="col-md-6 col-0 mt-5">
+        <div class="col-lg-6 col-0 mt-5 poster">
           <AppPoster/>
         </div>
-        <div class="col-md-6 mt-5 d-flex flex-column justify-content-center align-items-center">
+        <div class="col-lg-6 col-12 mt-5 d-flex flex-column justify-content-center align-items-center">
             <router-view v-slot="{ Component }">
                 <component :is="Component" view-prop="value" />
             </router-view>
@@ -27,5 +27,9 @@ export default {
 
 
 <style lang="scss" scoped>
-
+@media (max-width: 992px) { 
+  .poster {
+    display: none;
+  }
+}
 </style>
