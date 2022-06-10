c205d8f5d2d2de3e9dee16cbe9d67fa417de00ae
hangvtt183524 <nanh.trang.140@gmail.com>
2022-06-11 00:05:53 +0700

    re-structure folder to fix stupid mistake

--- PetCare-Portal/src/App.vue
+++ PetCare-Portal/src/App.vue
@@ -1,60 +1,57 @@
-<script>
-import AppLayout from './general/AppLayout';
-
-export default {
-  name: "App",
-  components: {
-    AppLayout,
-  },
-};
-</script>
-
-
 <template>
-  <div id="app">
-    <div class="d-flex flex-column flex-root">
-      <div class="page d-flex flex-column flex-fluid">
-        <div class="wrapper d-flex flex-row flex-fluid">
-          <AppLayout>
-            <div class="bg-blue">
-              <router-view v-slot="{ Component }">
-                <component :is="Component" view-prop="value" />
-              </router-view>
-            </div>
-          </AppLayout>
-        </div>
-      </div>
+  <div className="app-wrapper">
+    <div className="app">
+      <TheNavigation/>
+      <router-view/>
     </div>
   </div>
 </template>
 
+<script>
+import TheNavigation from './views/components/Navigation'
+
+export default {
+  name: "app",
+  components: {
+    TheNavigation,
+  },
+  data() {
+    return {};
+  },
+  created() {
+  },
+  mounted() {
+  },
+  methods: {},
+  watch: {},
+};
+</script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");
-
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: "Quicksand", sans-serif;
-}
-
-#app{
-  display: flex;
-  height: 100vh;
-  overflow-x:hidden;
-  overflow-y:auto;
-}
-
-.flex-root {
-  flex: 1 1 0;
-}
-
-.flex-fluid {
-  flex: 1 0 0;
-}
-
-.bg-blue {
-  background-color: #a1d8ff7a;
-}
+// @import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");
+// * {
+//   margin: 0;
+//   padding: 0;
+//   box-sizing: border-box;
+//   font-family: "Quicksand", sans-serif;
+// }
+// .app {
+//   display: flex;
+//   flex-direction: column;
+//   min-height: 100vh;
+// }
+// .container {
+//   max-width: 1440px;
+//   margin: 0 auto;
+// }
+// .link {
+//   cursor: pointer;
+//   text-decoration: none;
+//   text-transform: uppercase;
+//   color: black;
+// }
+// .link-light {
+//   color: #fff;
+// }
+@import url('./styles/main.css');
 </style>
