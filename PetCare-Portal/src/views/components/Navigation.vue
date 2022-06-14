818d81b345622b53c0ec784e63a638de7d019f9b
NinhNghia <nghianinhnb@gmail.com>
2022-06-14 08:25:41 +0700

    chỉnh lại các components đã làm theo template cũ

--- PetCare-Portal/src/views/components/Navigation.vue
+++ PetCare-Portal/src/views/components/Navigation.vue
@@ -1,41 +1,73 @@
 <template lang="">
-    <div class="navigation">
-        <div class="navigation-tittle">Pet Care</div>
-        <div class="menu">
-            <div class="choice">
-                <router-link class="page-link" to="/">Trang chủ</router-link>
-                <div class="underline"></div>
+    <div class="container AppHeader">
+        <div class="row align-items-center">
+            <div class="col-3 py-3">
+                <router-link to="/home">
+                    <p class="AppLogo">Pet Care</p>
+                </router-link>
             </div>
-            <div class="choice">
-                <router-link class="page-link" to="/booking-schedule">Dịch vụ</router-link>
-                <div class="underline"></div>
+
+            <div class="col-6">
+                <div class="d-flex flex-row justify-content-start">
+                    <router-link v-for="tab in tabs" :to="tab.path" :key="tab.id" class="mx-4">
+                        <p class="AppHeader_Link Font_20" :class="{active : curPath === tab.path}">
+                            {{tab.text}}
+                        </p>
+                    </router-link>
+                </div>
             </div>
-            <div class="choice">
-                Sản phẩm
-                <div class="underline"></div>
-            </div>
-            <div class="choice">
-                Hỏi đáp
-                <div class="underline"></div>
+
+            <div class="col-3">
+                <div class="d-flex flex-row justify-content-end">
+                    <router-link to="/sign-up" class="mx-2">
+                        <p class="Font_20 text-capitalize" :class="{active: curPath === '/sign-up'}">
+                            Đăng ký
+                        </p>
+                    </router-link>
+                    <router-link to="/login" class="mx-2">
+                        <p class="Font_20 text-capitalize" :class="{active: curPath === '/login'}">
+                            Đăng nhập
+                        </p>
+                    </router-link>
+                </div>
             </div>
         </div>
-        <div class="account">
-            <div class="choice">
-                Đăng xuất
-                <div class="underline"></div>
-                </div>
-            <div class="choice">
-                Đăng Nhập
-                <div class="underline"></div>
-                </div>
-        </div>
+        
     </div>
 </template>
+
 <script>
 export default {
   name: 'TheNavigation',
+      data() {
+        return {
+            tabs: [
+                {
+                    id: 'home',
+                    text: 'Trang chủ',
+                    path: '/home',
+                },
+                {
+                    id: 'sktc',
+                    text: 'Sức khỏe thú cưng',
+                    path: '',
+                },
+                {
+                    id: 'forum',
+                    text: 'Forum',
+                    path: '',
+                }
+            ]
+        }
+    },
+    computed: {
+        curPath() {
+            return this.$route.path;
+        }
+    }
 }
 </script>
+
 <style scoped>
-@import url('../../assets/styles/layout/navigation.css');
+@import url('../../assets/styles/layout/navigation.scss');
 </style>
