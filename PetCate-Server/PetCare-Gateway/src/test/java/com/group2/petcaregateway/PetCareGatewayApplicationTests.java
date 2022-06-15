0f0e831c2bcc0de8ddb472a3d41c22441afff4e4
hangvtt183524 <nanh.trang.140@gmail.com>
2022-06-15 23:21:35 +0700

    create Gateway API service

---
+++ PetCate-Server/PetCare-Gateway/src/test/java/com/group2/petcaregateway/PetCareGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group2.petcaregateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCareGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
