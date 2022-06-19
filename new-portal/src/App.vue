39311a6a63b83282e8a56cdd164d73eac7f2b710
bva <hochochoc999@gmail.com>
2022-06-19 21:10:25 +0700

    add admin-portal

---
+++ new-portal/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div className="app-wrapper">
+    <div className="app">
+      <TheNavigation/>
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+import TheNavigation from "@/views/components/Navigation";
+export default {
+  name: "app",
+  components: {
+    TheNavigation,
+  },
+  data() {
+    return {};
+  },
+  created() {
+  },
+  mounted() {
+  },
+  methods: {},
+  watch: {},
+};
+</script>
+
+<style lang="scss">
+
+@import url('./assets/styles/main.css');
+</style>
