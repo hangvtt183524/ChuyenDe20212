b0f7d7a389defd22f3071bf3c2cc8128335c7e7a
hangvtt183524 <nanh.trang.140@gmail.com>
2022-06-24 21:48:32 +0700

    add repository connect to db

--- PetCare-Server/PetCare-Infotype/src/main/java/com/group2/petcareinfotype/repository/PetRepository.java
+++ PetCare-Server/PetCare-Infotype/src/main/java/com/group2/petcareinfotype/repository/PetRepository.java
@@ -1,2 +1,9 @@
-package com.group2.petcareinfotype.repository;public interface PetRepository {
+package com.group2.petcareinfotype.repository;
+
+import com.group2.petcareinfotype.entity.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PetRepository extends JpaRepository<Pet, Long> {
 }
--- PetCare-Server/PetCare-Infotype/src/main/java/com/group2/petcareinfotype/repository/UserRepository.java
+++ PetCare-Server/PetCare-Infotype/src/main/java/com/group2/petcareinfotype/repository/UserRepository.java
@@ -1,2 +1,15 @@
-package com.group2.petcareinfotype.repository;public interface UserRepository {
+package com.group2.petcareinfotype.repository;
+
+import com.group2.petcareinfotype.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query(value = "select * from user where username like %:username%", nativeQuery = true)
+    public Optional<User> findUserByUsername(@Param("username") String username);
 }
