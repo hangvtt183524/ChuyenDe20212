775c9f3fd1e0968646727b7a6a24f770cf15d781
bva <hochochoc999@gmail.com>
2022-06-26 23:46:03 +0700

    add Edit action in doctor management page in admin-portal

--- admin-portal/src/views/DoctorManagement.vue
+++ admin-portal/src/views/DoctorManagement.vue
@@ -15,17 +15,34 @@
               <th>Địa chỉ</th>
               <th>Chuyên ngành</th>
               <th>Password</th>
+              <th>Hành động</th>
             </tr>
           </thead>
           <tbody class="body-row">
-            <tr v-for="doctor in getRenderUsers(firstIndex, lastIndex)" :key="doctor.id">
-              <td>{{doctor.name}}</td>
-              <td>{{doctor.dob}}</td>
-              <td>{{doctor.phone}}</td>
-              <td>{{doctor.email}}</td>
-              <td>{{doctor.address}}</td>
-              <td>{{doctor.major}}</td>
-              <td>{{doctor.password}}</td>
+            <tr v-for="doctor in renderUsers" :key="doctor.id">
+              <template v-if="doctor.isEditting">
+                <td><input type="text" :value=doctor.name></td>
+                <td><input type="text" :value=doctor.dob></td>
+                <td><input type="text" :value=doctor.phone></td>
+                <td><input type="email" :value=doctor.email></td>
+                <td><input type="text" :value=doctor.address></td>
+                <td><input type="text" :value=doctor.major></td>
+                <td><input type="password" :value=doctor.password></td>
+                <td><a v-on:click="handleDoneClick(doctor)">Lưu</a></td>
+              </template>
+              <template v-else>
+                <td>{{doctor.name}}</td>
+                <td>{{doctor.dob}}</td>
+                <td>{{doctor.phone}}</td>
+                <td>{{doctor.email}}</td>
+                <td>{{doctor.address}}</td>
+                <td>{{doctor.major}}</td>
+                <td>{{doctor.password}}</td>
+                <td class="action">
+                  <a v-on:click="handleEditClick(doctor)">Sửa</a>
+                  <a href="#">Xoá</a>
+                </td>
+              </template>
             </tr>
           </tbody>
         </table>
@@ -52,37 +69,62 @@
       props: {
         numOfUsersPerPage: {
           type: Number,
-          default: 1,
+          default: 2,
         }
       },
       components: {
         pagination: Pagination
       },
       data() {
+        const users = this.getData().users;
+        const renderUsers = users.slice(0, Math.min(this.numOfUsersPerPage, this.getData().users.length))
+        renderUsers.forEach(user => {
+          user.isEditting = false;
+        })
+
         return {
-          users: this.getData().users,
-          // renderUsers: this.fetchData().users.slice(0, this.numOfUsersPerPage),
+          users,
+          renderUsers,
           len: this.getData().users.length,
           totalPage: Math.ceil(this.getData().users.length / this.numOfUsersPerPage),
           currentPage: 1,
           firstIndex: 0,
-          lastIndex: this.numOfUsersPerPage - 1
+          lastIndex: Math.min(this.numOfUsersPerPage - 1, this.getData().users.length - 1),
         }
       },
       methods: {
         getData() {
+          const users = [
+            {
+              id: "001",
+              name: "Bùi Việt Anh",
+              dob: "24/1/2000",
+              address: "4 Bạch Mai, Hai Bà Trưng, TP Hà Nội",
+              email: 'anh@gmail.com',
+              password: '',
+              major: '',
+            },
+            {
+              id: "002",
+              name: "Bùi Việt Anh",
+              dob: "24/1/2000",
+              address: "4 Bạch Mai, Hai Bà Trưng, TP Hà Nội",
+              email: 'anh@gmail.com',
+              password: '',
+              major: '',
+            },
+            {
+              id: "003",
+              name: "Bùi Việt Anh",
+              dob: "24/1/2000",
+              address: "4 Bạch Mai, Hai Bà Trưng, TP Hà Nội",
+              email: 'anh@gmail.com',
+              password: '',
+              major: '',
+            },
+          ]
           return {
-            users: [
-              {
-                id: "001",
-                name: "Bùi Việt Anh",
-                dob: "24/1/2000",
-                address: "4 Bạch Mai, Hai Bà Trưng, TP Hà Nội",
-                email: 'anh@gmail.com',
-                password: '',
-                major: '',
-              },
-            ]
+            users
           }
         },
   
@@ -92,14 +134,41 @@
           if (page == this.totalPage)
             this.lastIndex = this.len - 1
           else this.lastIndex = page * this.numOfUsersPerPage - 1;
+
+          const renderUsers = []
+          for (let i = this.firstIndex; i <= this.lastIndex; i++) {
+            renderUsers.push(this.users[i])
+          }
+
+
+          renderUsers.forEach(user => {
+            user.isEditting = false;
+          })
+          console.log(renderUsers)
+          this.renderUsers = renderUsers
+        },
+
+        handleEditClick(doctor) {
+          this.renderUsers[this.renderUsers.indexOf(doctor)].isEditting = true;
+          console.log(this.renderUsers)
+        },
+
+        handleDoneClick(doctor) {
+          this.renderUsers[this.renderUsers.indexOf(doctor)].isEditting = false;
         },
   
         getRenderUsers(first, last) {
+
           const renderUsers = []
           for (let i = first; i <= last; i++) {
             renderUsers.push(this.users[i])
           }
 
+          renderUsers.forEach(user => {
+            user.isEditting = false;
+          })
+          console.log(renderUsers)
+          this.renderUsers = renderUsers
           return renderUsers;
         }
       }
@@ -120,7 +189,7 @@
       font-size: 40px;
       color: $colorPrimary900;
     }
-  
+
     .table-wrapper {
       padding-left: 100px;
       padding-right: 100px;
@@ -129,7 +198,7 @@
         border: 1px solid #ddd;
         border-collapse: collapse;
         width: 100%;
-  
+
         .header-row {
           margin: 50px;
           background-color: #63a2d8;
@@ -155,6 +224,10 @@
             padding-left: 10px;
             padding-top: 10px;
             padding-bottom: 10px;
+          }
+
+          .action {
+            padding-right: 15px;
           }
         }
   
