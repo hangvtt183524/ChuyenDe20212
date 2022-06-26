b59e57fa5b852b3a44e4ed68c00bf81a70904565
thule129 <baekhyunee129@gmail.com>
2022-06-26 17:22:30 +0700

    edit table in pet care history

--- PetCare-Portal/src/views/PetHistory.vue
+++ PetCare-Portal/src/views/PetHistory.vue
@@ -5,92 +5,39 @@
             <table class="table" ref="table">
                 <thead>
                     <tr>
-                        <th>STT</th>
+                        <th class="align-center">STT</th>
                         <th>Tên Bệnh</th>
                         <th>Bác sĩ</th>
-                        <th>thời gian</th>
-                        <th>Địa điểm</th>
+                        <th class="align-center">Thời gian</th>
+                        <th>Kết luận</th>
+                        <th>Chức năng</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>  
-                        <td>1</td>
+                        <td class="align-center">1</td>
                         <td>Rụng lông</td>
                         <td>Nguyễn Văn A</td>
-                        <td>09-12-2022</td>
-                        <td>Bệnh viện thú y HN</td>
-                    </tr>  
-                    <tr>  
-                        <td>1</td>
-                        <td>Rụng lông</td>
-                        <td>Nguyễn Văn A</td>
-                        <td>09-12-2022</td>
-                        <td>Bệnh viện thú y HN</td>
-                    </tr> 
-                    <tr>  
-                        <td>1</td>
-                        <td>Rụng lông</td>
-                        <td>Nguyễn Văn A</td>
-                        <td>09-12-2022</td>
-                        <td>Bệnh viện thú y HN</td>
-                    </tr> 
-                    <tr>  
-                        <td>1</td>
-                        <td>Rụng lông</td>
-                        <td>Nguyễn Văn A</td>
-                        <td>09-12-2022</td>
-                        <td>Bệnh viện thú y HN</td>
-                    </tr> 
-                    <tr>  
-                        <td>1</td>
-                        <td>Rụng lông</td>
-                        <td>Nguyễn Văn A</td>
-                        <td>09-12-2022</td>
-                        <td>Bệnh viện thú y HN</td>
-                    </tr> 
-                    <tr>  
-                        <td>1</td>
-                        <td>Rụng lông</td>
-                        <td>Nguyễn Văn A</td>
-                        <td>09-12-2022</td>
-                        <td>Bệnh viện thú y HN</td>
-                    </tr> 
-                    <tr>  
-                        <td>1</td>
-                        <td>Rụng lông</td>
-                        <td>Nguyễn Văn A</td>
-                        <td>09-12-2022</td>
-                        <td>Bệnh viện thú y HN</td>
-                    </tr> 
-                    <tr>  
-                        <td>1</td>
-                        <td>Rụng lông</td>
-                        <td>Nguyễn Văn A</td>
-                        <td>09-12-2022</td>
-                        <td>Bệnh viện thú y HN</td>
-                    </tr> 
-                    <tr>  
-                        <td>1</td>
-                        <td>Rụng lông</td>
-                        <td>Nguyễn Văn A</td>
-                        <td>09-12-2022</td>
-                        <td>Bệnh viện thú y HN</td>
-                    </tr> 
-                    <tr>  
-                        <td>1</td>
-                        <td>Rụng lông</td>
-                        <td>Nguyễn Văn A</td>
-                        <td>09-12-2022</td>
-                        <td>Bệnh viện thú y HN</td>
-                    </tr> 
+                        <td class="align-center">09-12-2022</td>
+                        <td>Đã chữa khỏi</td>
+                        <td>
+                            <Button
+                                text="Chi tiết"
+                                color="blue"
+                            />
+                        </td>
+                    </tr>
                 </tbody>
             </table>
         </div>
     </div>
 </template>
 <script>
+import Button from './components/Button.vue'
 export default {
-    
+    components:{
+        Button, 
+    }
 }
 </script>
 <style lang="">
