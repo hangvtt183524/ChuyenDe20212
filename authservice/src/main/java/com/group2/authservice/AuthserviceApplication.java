d2b8dc066254f11ee46a58366983d9cc0904849b
NGHIA.NV190060@sis.hust.edu.vn <NGHIA.NV190060@sis.hust.edu.vn>
2022-06-30 10:29:48 +0700

    AUTH 001

---
+++ authservice/src/main/java/com/group2/authservice/AuthserviceApplication.java
@@ -0,0 +1,13 @@
+package com.group2.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthserviceApplication.class, args);
+	}
+
+}
